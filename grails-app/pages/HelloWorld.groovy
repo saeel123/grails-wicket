aef89e51f097e000bcd97ab62f18be3ab8179824
saeel123 <saeelmacman123@gmail.com>
2020-10-16 17:56:02 +0530

    added hello world and click counter

--- grails-app/pages/HelloWorld.groovy
+++ grails-app/pages/HelloWorld.groovy
@@ -1,10 +1,37 @@
 
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.basic.Label
+import org.apache.wicket.markup.html.link.Link
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink
+import org.apache.wicket.ajax.AjaxRequestTarget
+import org.apache.wicket.model.PropertyModel
+
+
 import org.apache.wicket.markup.html.WebPage
 
 
 public class HelloWorld extends WebPage {
+    private int counter = 0
+    private Label label
+
     public HelloWorld() {
+    
         add(new Label("message", "hello World Saeel"));
+
+        add(new AjaxFallbackLink("link") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                counter++;
+                if(target != null) {
+                    target.addComponent(label)
+                }
+            }
+        });
+        
+        label = new Label("label", new PropertyModel(this, "counter"));
+        label.setOutputMarkupId(true);
+        add(label)
+
+        
+
     }
 }
